fb6728dd45211092fed9220579cddd970d41b000
SamCook42 <gitsam@samlr.com>
2014-08-17 13:23:28 +0100

    get menu_bar function producing reasonable result

--- answer/static/js/components/menu-bar.js
+++ answer/static/js/components/menu-bar.js
@@ -4,3 +4,74 @@
  * clicked will be displayed.
  */
 
+menu_bar = function () {
+    'use strict';
+
+    function _menu_generator (parent, config, depth) {
+        var child_node, sub_node, item, depth_str,
+            len = config.length;
+        depth = depth + 1 || 0;
+        depth_str = depth.toString(10);
+
+        for (var i = 0; i < len; i++) {
+            item = config[i];
+            child_node = document.createElement('li');
+            child_node.innerHTML = item.name;
+            child_node.classList.add('menu__item--'+depth_str);
+
+            if (item.children) {
+                sub_node = document.createElement('ul');
+                _menu_generator(sub_node, item.children, depth);
+                child_node.appendChild(sub_node);
+            } else {
+                sub_node = document.createElement('span');
+                sub_node.innerHTML = item.content;
+                child_node.appendChild(sub_node);
+            }
+
+            parent.appendChild(child_node);
+        }
+    }
+
+    function _start_it (parentNode, configuration) {
+        var topElement = document.createElement('ul');
+        topElement.classList.add('menu__root');
+        parentNode.appendChild(topElement);
+        _menu_generator(topElement, configuration);
+    }
+
+    // The top level function, checks the selector checks the configuration
+    function _menu_bar (parentSelector, configuration) {
+        var parentNode;
+
+        // Approximation to CSS style selectors
+        if (/^\./.test(parentSelector)) {
+            parentSelector = parentSelector.substring(1); 
+            parentNode = document.getElementsByClassName(parentSelector);
+        } else if (/^#/.test(parentSelector)) {
+            parentSelector = parentSelector.substring(1);
+            parentNode = document.getElementById(parentSelector);
+        } else {
+            parentNode = document.getElementByTagName(parentSelector);
+        }
+
+        if ( !parentNode ) {
+            console.error('No parent node found for: ', parentSelector);
+            return;
+        } else if (parentNode.length !== undefined) {
+            console.warn('Found multiple nodes for: ', parentSelector, 'using first');
+            parentNode = parentNode[0];
+        }
+
+        if (typeof configuration === 'string' && loader) {
+            // if it's a string assume it's a path to a config and load it
+            loader(configuration, function (loaded) {
+                _start_it(parentNode, configuration);
+            });
+        } else {
+            _start_it(parentNode, configuration);
+        }
+    }
+
+    return _menu_bar;
+}();
